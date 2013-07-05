e468b146ae84845043c47189daedfcc108ab0ccd
Muhammad Usman <mdusyl@yahoo.com>
2013-07-05 22:32:51 +0600

    More behaviour testing.

--- index.php
+++ index.php
@@ -14,23 +14,24 @@ new Pixie\Connection('mysql', array(
 
 $query = DB::table('my_table')
     ->where('my_table.id', '>', 1)
-    //->orWhere('id', '=', 1)
+    ->orWhere('my_table.id', '=', 1)
     ->where(function($q)
         {
             $q->where('value', 'LIKE', '%sana%');
             $q->orWhere(function($q2)
                 {
                     $q2->where('key', 'LIKE', '%sana%');
+                    $q2->orWhere('value', 'LIKE', '%sana%');
                 });
         })
-    /*->join(DB::raw('cb_person_details as a'), DB::raw('a.person_id'), '=', 'my_table.id')
+    ->join(DB::raw('cb_person_details as a'), DB::raw('a.person_id'), '=', 'my_table.id')
 
-    ->join(DB::raw('cb_person_details as b'), function($table)
+    ->leftJoin(DB::raw('cb_person_details as b'), function($table)
         {
             $table->on(DB::raw('b.person_id'), '=', 'my_table.id');
             $table->orOn(DB::raw('b.age'), '>', DB::raw(1));
-        }, null, null, 'left')*/
-
+        })
 ;
 
+var_dump($query->getQuery()->getRawSql());
 var_dump($query->get());
--- tests/Pixie/QueryBuilderTest.php
+++ tests/Pixie/QueryBuilderTest.php
@@ -12,6 +12,7 @@ class QueryBuilderTest extends TestCase
 
         $this->builder = new QueryBuilder\QueryBuilderHandler($this->mockConnection);
     }
+
     public function testSelectQuery()
     {
         $subQuery = $this->builder->table('person_details')->select('details')->where('person_id', '=', 3);
@@ -37,9 +38,38 @@ class QueryBuilderTest extends TestCase
         ;
 
         $nestedQuery = $this->builder->table($this->builder->subQuery($query, 'bb'))->select('*');
-        //$query = DB::select('*')->from('cb_my_table')->whereNotIn('id', array(1))->get();
+        //$query = $this->builder->select('*')->from('cb_my_table')->whereNotIn('id', array(1))->get();
         $this->assertEquals("SELECT * FROM (SELECT `cb_my_table`.*, count(cb_my_table.id) as tot, (SELECT `details` FROM `cb_person_details` WHERE `person_id` = 3) as pop FROM `cb_my_table` INNER JOIN `cb_person_details` ON `cb_person_details`.`person_id` = `cb_my_table`.`id` WHERE `value` = 'Ifrah' AND `cb_my_table`.`id` IN (1, 2) GROUP BY `value`, `cb_my_table`.`id`, `cb_person_details`.`id` HAVING `tot` < 2 ORDER BY `cb_my_table`.`id` DESC,`value` ASC LIMIT 1 OFFSET 0) as bb"
             , $nestedQuery->getQuery()->getRawSql());
+    }
+
+    public function testSelectQueryWithNestedCriteriaAndJoins()
+    {
+        $builder = $this->builder;
+
+        $query = $this->builder->table('my_table')
+            ->where('my_table.id', '>', 1)
+            ->orWhere('my_table.id', '=', 1)
+            ->where(function($q)
+                {
+                    $q->where('value', 'LIKE', '%sana%');
+                    $q->orWhere(function($q2)
+                        {
+                            $q2->where('key', 'LIKE', '%sana%');
+                            $q2->orWhere('value', 'LIKE', '%sana%');
+                        });
+                })
+            ->join($this->builder->raw('cb_person_details as a'), $this->builder->raw('a.person_id'), '=', 'my_table.id')
+
+            ->leftJoin($this->builder->raw('cb_person_details as b'), function($table) use ($builder)
+                {
+                    $table->on($builder->raw('b.person_id'), '=', 'my_table.id');
+                    $table->orOn($builder->raw('b.age'), '>', $builder->raw(1));
+                })
+        ;
+
+        $this->assertEquals("SELECT * FROM `cb_my_table` INNER JOIN cb_person_details as a ON a.person_id = `cb_my_table`.`id` LEFT JOIN cb_person_details as b ON b.person_id = `cb_my_table`.`id` OR b.age > 1 WHERE `cb_my_table`.`id` > 1 OR `cb_my_table`.`id` = 1 AND (`value` LIKE '%sana%' OR (`key` LIKE '%sana%' OR `value` LIKE '%sana%'))"
+            , $query->getQuery()->getRawSql());
     }
 
     public function testInsertQuery()
