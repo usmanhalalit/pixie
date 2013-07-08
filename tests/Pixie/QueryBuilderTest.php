a28cb04aeced7e95a61077ed31e25be766746d63
Muhammad Usman <mdusyl@yahoo.com>
2013-07-08 20:19:09 +0600

    QueryBuilderTest added.

---
+++ tests/Pixie/QueryBuilderTest.php
@@ -0,0 +1,26 @@
+<?php namespace Pixie;
+
+use Mockery as m;
+use Pixie\QueryBuilder\QueryBuilderHandler;
+
+class QueryBuilder extends TestCase
+{
+    /**
+     * @var QueryBuilderHandler
+     */
+    protected $builder;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->builder = new QueryBuilderHandler($this->mockConnection);
+    }
+
+    public function testRawQuery()
+    {
+        $query = 'select * from cb_my_table where id = ? and name = ?';
+        $bindings = array(5, 'usman');
+        $queryArr = $this->builder->query($query, $bindings)->get();
+        $this->assertEquals($queryArr, array($query, $bindings));
+    }
+}
