2fd4869a1ea448d2344c990c5b7dbe3366884f52
TCB13 <tcb13@iklive.org>
2015-03-17 01:21:55 +0000

    UPDATE and DELETE now escape table names as well
    Fixes issue where UPDATE and DELETE queries didn't escape table names. This could lead to issues when table names included special characters like a backslash. The issue was by passing the table name into the already existing wrapSanitizer() method used by select queries.

--- src/Pixie/QueryBuilder/Adapters/BaseAdapter.php
+++ src/Pixie/QueryBuilder/Adapters/BaseAdapter.php
@@ -268,7 +268,7 @@ abstract class BaseAdapter
 
         $sqlArray = array(
             'UPDATE',
-            $table,
+            $this->wrapSanitizer($table),
             'SET ' . $updateStatement,
             $whereCriteria,
             $limit
@@ -302,7 +302,7 @@ abstract class BaseAdapter
         // Limit
         $limit = isset($statements['limit']) ? 'LIMIT ' . $statements['limit'] : '';
 
-        $sqlArray = array('DELETE from', $table, $whereCriteria, $limit);
+        $sqlArray = array('DELETE FROM', $this->wrapSanitizer($table), $whereCriteria);
         $sql = $this->concatenateQuery($sqlArray, ' ', false);
         $bindings = $whereBindings;
 
@@ -402,7 +402,7 @@ abstract class BaseAdapter
                         }
 
                         $valuePlaceholder = trim($valuePlaceholder, ', ');
-                        $criteria .= ' (' . $valuePlaceholder . ') ';
+                   d     $criteria .= ' (' . $valuePlaceholder . ') ';
                         break;
                 }
             } else {
